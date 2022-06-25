6afd1b7ac24bb8b8e1ee68b3e59fd15a312d6b77
bloeys <bloeys@outlook.com>
2022-06-25 23:58:57 +0400

    Fix msg

--- wavy.go
+++ wavy.go
@@ -283,7 +283,7 @@ func (s *Sound) Close() error {
 func CopyInMemSound(s *Sound) *Sound {
 
 	if s.Info.Mode != SoundMode_Memory {
-		panic("only in-memory sounds can not be copied. Please use NewSoundStreaming if you want to have multiple sound objects of a streaming sound")
+		panic("only in-memory sounds can be copied. Please use NewSoundStreaming if you want to have multiple sound objects of a streaming sound")
 	}
 
 	d := s.Data.(*SoundBuffer).Copy()
